ef973024f227cccd63801cefe0ab8c67066eabb1
Thanos-Skoulo <t.skoulo@gmail.com>
2019-04-03 20:25:42 +0300

    add two toasts (with delay between them)

--- app/src/main/java/thanos/skoulopoulos/gr/myfirstapp/TasksActivity.java
+++ app/src/main/java/thanos/skoulopoulos/gr/myfirstapp/TasksActivity.java
@@ -1,6 +1,8 @@
 package thanos.skoulopoulos.gr.myfirstapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class TasksActivity extends AppCompatActivity {
@@ -59,10 +62,33 @@ public class TasksActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+
+
         if (requestCode == ADD_TASK){
             if(resultCode == RESULT_OK){
-                
+
+                Context contextTask = getApplicationContext();
+                final Context contextDiscription = getApplicationContext();
+                CharSequence textTask = data.getStringExtra(TASK_CREATION);
+                final CharSequence textDiscription = data.getStringExtra(DESCRIPTION_CREATION);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toastTask = Toast.makeText(contextTask,textTask , duration);
+                toastTask.show();
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toastTask = Toast.makeText(contextDiscription, textDiscription, duration);
+                        toastTask.show();
+                    }
+                    },2000);
+                }
+
             }
         }
-    }
 }
+
+
